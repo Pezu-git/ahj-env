3f7170feed4a5a3ac12d840ee3973d43d4e3f6af
Pezu-git <artem_nikolaev69@mail.ru>
2021-05-03 00:39:06 +0300

    complete

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,7 +2,7 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import demo from '../js/app.js';
+import demo from '../src/js/app.js';
 
 test('waits 1 second before ending the game', () => {
   expect(demo(3)).toBe(3);
